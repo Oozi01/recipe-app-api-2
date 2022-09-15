28c7da3ca737201f0ecd51e87ca25b2c59a3cda9
Oskar <oskar.ciesla@interia.pl>
2022-09-15 13:58:10 +0200

    Updating DataBase.

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions.
+"""
+
+
+def add(x, y):
+    """Add x and y - return result[int]"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract y from x - return result[int]"""
+    return x - y
