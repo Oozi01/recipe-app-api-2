e508a5148bc8e20dd5c0c9bfd9eace6259184269
Oskar <oskar.ciesla@interia.pl>
2022-10-07 21:53:00 +0200

    Configuring setting and urls for images purpose.

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'core',
     'user',
     'recipe',
-    
+
     'rest_framework',
     'rest_framework.authtoken',
     'drf_spectacular',
@@ -127,7 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
